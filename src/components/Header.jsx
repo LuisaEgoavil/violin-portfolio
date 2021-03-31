9ba451d978bbd9beb469b17455e8e33c37daec69
Luisa Egoavil <lupi@Luisas-Air.fritz.box>
2021-03-31 12:39:54 +0200

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+function Header () {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Über mich</li>
+                    <li>Vita</li>
+                    <li>Unterricht</li>
+                    <li>Kontakt</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
